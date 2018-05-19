e4800285481b82fc5c45e45e807554fe9fd75536
isoki87 <helloleolin@gmail.com>
2018-05-18 18:45:49 -0700

    Update SudokuBoard.java

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,3 +1,5 @@
+package com.leo.sudoku;
+
 public class SudokuBoard {
     private int[][] board;
 
@@ -13,44 +15,29 @@ public class SudokuBoard {
         return board[x][y];
     }
 
+    public boolean checkAnswer(int[][] ans){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                if(ans[i][j] != board[i][j]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    public int[][] showBoard(){
+    	return board;
+    }
+    
+    public int[] showColumn(int row){
+    	return board[row];
+    }
+    
     public void displayBoard(){
         coverInternal(0);
     }
-
-    public void cvr2Play(String diff){
-        int diffLvl = 0;
-        switch(diff.toLowerCase()){
-            case("easy"): diffLvl = 1;
-                break;
-            case("medium"): diffLvl = 2;
-                break;
-            case("hard"): diffLvl = 3;
-                break;
-        }
-        coverInternal(diffLvl);
-    }
-
-    private int detBlanks(int lvl){
-        int blanks = 0;
-        switch(lvl){
-            case 1: blanks = 40;
-                break;
-            case 2: blanks = 33;
-                break;
-            case 3: blanks = 28;
-                break;
-        }
-        return blanks;
-    }
-
-    private boolean isBlank(int blanks, int spaces){
-        double prob = ((double)(blanks)) / ((double)(spaces));
-        if(prob >= Math.random()){
-            return true;
-        }
-        return false;
-    }
-
+    
     private void coverInternal(int lvl){
         int blanks = detBlanks(lvl);
         int spaces = 81;
@@ -68,15 +55,26 @@ public class SudokuBoard {
             System.out.println("\n");
         }
     }
-
-    public boolean checkAnswer(int[][] ans){
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
-                if(ans[i][j] != board[i][j]){
-                    return false;
-                }
-            }
+    
+    private boolean isBlank(int blanks, int spaces){
+        double prob = ((double)(blanks)) / ((double)(spaces));
+        if(prob >= Math.random()){
+            return true;
         }
-        return true;
+        return false;
     }
+    
+    private int detBlanks(int lvl){
+        int blanks = 0;
+        switch(lvl){
+            case 1: blanks = 40;
+                break;
+            case 2: blanks = 33;
+                break;
+            case 3: blanks = 28;
+                break;
+        }
+        return blanks;
+    }
+    
 }
