542b3287c650c0442cb3d794751ac6d7de81ebb4
isoki87 <helloleolin@gmail.com>
2018-05-18 18:45:34 -0700

    Update and rename SudokuSolver.java to SudokuGenerator.java

--- SudokuGenerator.java
+++ SudokuGenerator.java
@@ -1,16 +1,14 @@
-public class SudokuSolver {
-	
-  public int[][] solvePuzzle(int[][] unsolved){
-    int[][] board = new int[9][9];
-    copyBoard(board, unsolved);
-    solveInternal(board);
-    return board;
-  }
-	
-  public boolean isSolved(int[][] puzzle){
+package com.leo.sudoku;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SudokuGenerator {
+    
+  public boolean isSolved(SudokuBoard puzzle){
     for(int i = 0; i < 9; i++){
       for(int j = 0; j < 9; j++){
-        if(!sudCheck(puzzle, puzzle[i][j], i, j)){
+        if(!sudCheck(puzzle, puzzle.check(i, j), i, j)){
           return false;
         }
       }
@@ -18,19 +16,19 @@ public class SudokuSolver {
     return true;
   }
 	
-  private boolean solveInternal(int[][] board){
+  public boolean populateBoard(SudokuBoard board){
     ArrayList<Integer> numList = createNumList();
     for(int row = 0; row < 9; row++){
       for(int column = 0; column < 9; column++){
-        if(board[row][column] == 0){
+        if(board.check(row, column) == 0){
           Collections.shuffle(numList);
           for(int i = 0; i < 9; i++){
             if(sudCheck(board, numList.get(i), row, column)){
-              board[row][column] = numList.get(i);
-              if(solveInternal(board)){
+              board.add(numList.get(i), row, column);
+              if(populateBoard(board)){
                 return true;
               } else {
-                board[row][column] = 0;
+                board.add(0, row, column);
               }
             }
           }
@@ -43,13 +41,13 @@ public class SudokuSolver {
   
   private ArrayList<Integer> createNumList(){
     ArrayList<Integer> list = new ArrayList<>();
-    for(int i = 0; i < list.size(); i++){
+    for(int i = 0; i < 9; i++){
       list.add(i + 1);
     }
     return list;
   }
 	
-  private boolean sudCheck(int[][] board, int num, int row, int column){
+  private boolean sudCheck(SudokuBoard board, int num, int row, int column){
     if(checkRow(board, num, row)){
       if(checkColumn(board, num, column)){
         if(checkQuadrant(board, num, row, column)){
@@ -60,32 +58,32 @@ public class SudokuSolver {
     return false;
   }
 	
-  private boolean checkRow(int[][] board, int num, int row){
+  private boolean checkRow(SudokuBoard board, int num, int row){
     for(int i = 0; i < 9; i++){
-      if(board[row][i] == num){
+      if(board.check(row, i) == num){
         return false;
       }
     }
     return true;		
   }
 	
-  private boolean checkColumn(int[][] board, int num, int column){
+  private boolean checkColumn(SudokuBoard board, int num, int column){
     for(int i = 0; i < 9; i++){
-      if(board[i][column] == num){
+      if(board.check(i, column) == num){
         return false;
       }
     }
     return true;
   }
 	
-  private boolean checkQuadrant(int[][] board, int num, int row, int column){
+  private boolean checkQuadrant(SudokuBoard board, int num, int row, int column){
     int rowQuad = row / 3;
     int colQuad = column / 3;
     for(int i = 0; i < 9; i++){
       if((i / 3) == rowQuad){
         for(int j = 0; j < 9; j++){
           if((j / 3) == colQuad){
-            if(board[i][j] == num){
+            if(board.check(i, j) == num){
               return false;
             }
           }
@@ -95,11 +93,4 @@ public class SudokuSolver {
     return true;
   }
 	
-  private void copyBoard(int[][] board, int[][] puzzle){
-    for(int i = 0; i < puzzle.length; i++){
-      for(int j = 0; j < puzzle.length; j++){
-        board[i][j] = puzzle[i][j];
-      }
-    }
-  }
 }
