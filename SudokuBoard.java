b78ebfd97ec769b685e0fc900b1c31cbb803873e
Leo <helloleolin@gmail.com>
2019-03-18 00:17:42 -0700

    Added comments for clarification

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -7,18 +7,22 @@ public class SudokuBoard {
         this.board = new int[9][9];
     }
 
+    //Set the number at the given position
     public void add(int num, int x, int y){
         board[x][y] = num;
     }
 
+    //Returns the number at the given position
     public int check(int x, int y){
         return board[x][y];
     }
    
+    //Returns board as a 2d array
     public int[][] showBoard(){
     	return board;
     }
     
+    //Show an entire column
     public int[] showColumn(int row){
     	return board[row];
     }
